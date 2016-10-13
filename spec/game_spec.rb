3aacc57b802952c97b2d2a44e4fa493ae2fcc4e7
crispinandrews <crispin.andrews@gmail.com>
2016-10-13 10:54:40 +0100

    part way through stage 26

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,4 +11,18 @@ describe Game do
       game.attack(player2)
     end
   end
+
+  describe '#player_1' do
+    it 'retrieves the first player' do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+  describe '#player_2' do
+    it 'retrieves the second player' do
+      expect(game.player_2).to eq player_2
+    end
+  end
+
+
 end
